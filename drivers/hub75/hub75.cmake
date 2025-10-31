3988a19bac97f8e7c3b239281d40a4df8ba3f309
Markus Kalkbrenner <git@kalki.de>
2025-10-31 16:24:39 +0100

    Change option for pico_graphics support in hub75

--- drivers/hub75/hub75.cmake
+++ drivers/hub75/hub75.cmake
@@ -1,4 +1,4 @@
-option(NO_PICO_GRAPHICS "Option to build shared library without pico_graphics support" OFF)
+option(ENABLE_PICO_GRAPHICS "Option to build shared library with or without pico_graphics support" ON)
 
 add_library(hub75 INTERFACE)
 
@@ -10,8 +10,9 @@ pico_generate_pio_header(hub75 ${CMAKE_CURRENT_LIST_DIR}/hub75.pio)
 target_include_directories(hub75 INTERFACE ${CMAKE_CURRENT_LIST_DIR})
 
 # Pull in pico libraries that we need
-if(NO_PICO_GRAPHICS)
-    target_link_libraries(hub75 INTERFACE pico_stdlib hardware_pio hardware_dma)
-else ()
+if(ENABLE_PICO_GRAPHICS)
     target_link_libraries(hub75 INTERFACE pico_stdlib hardware_pio hardware_dma pico_graphics)
+else ()
+    target_compile_options(hub75 INTERFACE -DNO_PICO_GRAPHICS)
+    target_link_libraries(hub75 INTERFACE pico_stdlib hardware_pio hardware_dma)
 endif ()
